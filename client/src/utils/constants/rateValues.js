ea06786ab55da655bb836f1098866964c436fef6
Belka-S <belka.srh.it@gmail.com>
2024-12-18 18:35:49 +0100

    update: speakRates

--- client/src/utils/constants/rateValues.js
+++ client/src/utils/constants/rateValues.js
@@ -1,9 +1,13 @@
 export const rateValues = [
-  { value: 0.25, label: 'Rate: 0.25' },
+  { value: 0.2, label: 'Rate: 0.2' },
+  { value: 0.3, label: 'Rate: 0.3' },
+  { value: 0.4, label: 'Rate: 0.4' },
   { value: 0.5, label: 'Rate: 0.5' },
-  { value: 0.75, label: 'Rate: 0.75' },
+  { value: 0.6, label: 'Rate: 0.6' },
+  { value: 0.7, label: 'Rate: 0.7' },
+  { value: 0.8, label: 'Rate: 0.8' },
+  { value: 0.9, label: 'Rate: 0.9' },
   { value: 1, label: 'Rate: 1' },
   { value: 1.25, label: 'Rate: 1.25' },
   { value: 1.5, label: 'Rate: 1.5' },
-  { value: 1.75, label: 'Rate: 1.75' },
 ];
