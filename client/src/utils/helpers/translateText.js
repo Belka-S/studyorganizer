ff0275ae89f172eac4e881ded87112494cb368f5
Belka-S <belka.srh.it@gmail.com>
2025-01-03 18:31:05 +0100

    update: translateText

--- client/src/utils/helpers/translateText.js
+++ client/src/utils/helpers/translateText.js
@@ -5,10 +5,20 @@ translate.engine = 'google'; // "google", "yandex", "libre", "deepl"
 
 export const translateText = async (text, { from, to }) => {
   try {
-    const translation = await translate(`${text}`, { from, to });
-    return translation.split(/\s+/).length !== 1
-      ? translation
-      : translation.at(0).toLocaleLowerCase() + translation.substring(1);
+    let translation = await translate(`${text}`, { from, to });
+    if (translation.split(/\s+/).length === 1) {
+      translation = translation.toLocaleLowerCase();
+    }
+    if (to.includes('en') && text.toLowerCase().includes('uhr')) {
+      translation = translation
+        .replaceAll('a.m. on', 'am on')
+        .replaceAll('p.m. on', 'pm on')
+        .replaceAll('a.m. to', 'am to')
+        .replaceAll('p.m. to', 'pm to')
+        .replaceAll('a.m.', 'am.')
+        .replaceAll('p.m.', 'pm.');
+    }
+    return translation;
   } catch (err) {
     return err.message;
   }
