b53bf20bb08ac69279d20e6f5a609c61a33f2e5a
Belka-S <Belka-S@users.noreply.github.com>
2025-01-31 09:03:21 +0000

    deploy: d7f307fa91b410c2ff6a51ff61187aa7b10a23bd

--- assets/VerifyForm-c9d992bb.js
+++ assets/VerifyForm-c9d992bb.js
@@ -1,4 +1,4 @@
-import{n as u,s as c,t as x,L as f,P as s,j as e,G as g,d as m,F as j,u as b,r as F,v as y,Q as C,g as v}from"./index-1c76ad4a.js";import{M as $}from"./index-0dba4ed6.js";import{f as k,F as T,a as L,T as V,L as z,E,b as S,c as w,d as M,S as O,e as R}from"./AuthForms.styled-9926def6.js";import{v as B}from"./clusterSchema-be4365ef.js";const{colors:G}=x,l=u`
+import{n as u,s as c,t as x,L as f,P as s,j as e,G as g,d as m,F as j,u as b,r as F,v as y,Q as C,g as v}from"./index-5e9dbd37.js";import{M as $}from"./index-12edbf07.js";import{f as k,F as T,a as L,T as V,L as z,E,b as S,c as w,d as M,S as O,e as R}from"./AuthForms.styled-2386270d.js";import{v as B}from"./clusterSchema-5abc5245.js";const{colors:G}=x,l=u`
   margin-left: auto;
 
   color: ${G.accent};
